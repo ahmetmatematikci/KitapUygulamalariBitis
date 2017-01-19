e7d55fd47ea857f2c560ee8a0e541b2d324e14ac
ahmetmatematikci <imdahmetkilic@gmail.com>
2017-01-19 15:47:46 +0300

    deneme

---
+++ app/src/main/java/com/ahmetmatematikci/kitapuygulamalari/CustomListView.java
@@ -0,0 +1,17 @@
+package com.ahmetmatematikci.kitapuygulamalari;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by a on 1/19/17.
+ */
+
+public class CustomListView extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.standartlistview);
+    }
+}
