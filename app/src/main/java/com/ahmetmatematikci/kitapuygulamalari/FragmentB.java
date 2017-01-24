d9124ac87305c238b04dd03b8175ca6a1f38d462
ahmetmatematikci <imdahmetkilic@gmail.com>
2017-01-24 13:04:29 +0300

    Fragmentler eklendi Flipper eklendş

--- app/src/main/java/com/ahmetmatematikci/kitapuygulamalari/FragmentB.java
+++ app/src/main/java/com/ahmetmatematikci/kitapuygulamalari/FragmentB.java
@@ -6,7 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 /**
  * Created by a on 1/24/17.
@@ -37,6 +39,38 @@ public class FragmentB extends Fragment {
         super.onActivityCreated(savedInstanceState);
         TextView tv = (TextView) getActivity().findViewById(R.id.textView23);
         tv.setText(isim);
+        ViewFlipper flipper = (ViewFlipper)getActivity().findViewById(R.id.fliper);
+        ImageView image1 = (ImageView)getActivity().findViewById(R.id.imageView4);
+        ImageView image2 = (ImageView)getActivity().findViewById(R.id.imageView5);
+        ImageView image3 = (ImageView)getActivity().findViewById(R.id.imageView6);
+
+        if(isim.equals("At")) {
+
+            image1.setImageResource(R.mipmap.a);
+            image2.setImageResource(R.mipmap.b);
+            image3.setImageResource(R.mipmap.c);
+
+
+        } else if (isim.equals("Kaplan")) {
+            image1.setImageResource(R.mipmap.d);
+            image2.setImageResource(R.mipmap.e);
+            image3.setImageResource(R.mipmap.f);
+
+        } else if (isim.equals("Fil")) {
+
+            image1.setImageResource(R.mipmap.g);
+            image2.setImageResource(R.mipmap.h);
+            image3.setImageResource(R.mipmap.l);
+
+        }else if (isim.equals("Kedi")) {
+            image1.setImageResource(R.mipmap.m);
+            image2.setImageResource(R.mipmap.n);
+            image3.setImageResource(R.mipmap.r);
+
+
+        }
+        flipper.setFlipInterval(2000);
+        flipper.startFlipping();
 
     }
 
